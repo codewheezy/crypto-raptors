203414a7ee9a493be8222381c90f1cc64e928bcc
codewheezy <uchennaibe95@gmail.com>
2022-03-28 18:02:25 +0100

    added header and hero components

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'black-rgba': 'rgba(4, 17, 29, 0.75)',
+      },
+    },
+  },
+  plugins: [],
+}
